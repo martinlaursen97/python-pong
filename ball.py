b353e819d7a4a7620e9539b95e468a4abd479ff1
martinlaursen97 <martin_laursen21@hotmail.com>
2022-08-15 23:41:06 +0200

    ball collision fix

--- ball.py
+++ ball.py
@@ -11,38 +11,41 @@ class Ball:
 
     def update(self):
         self.check_collisions()
-        self.move()
         self.draw()
 
-    def move(self):
-        self.position += self.direction
+    def move(self, next_x, next_y):
+        self.position.x = next_x
+        self.position.y = next_y
 
     def draw(self):
         pygame.draw.circle(self.display, (255, 255, 255), self.position, self.size)
 
     def check_collisions(self):
 
+        dir_norm = self.direction.normalize()
+        next_x = self.position.x
+        next_y = self.position.y
+
         # check for collision between current and next position
         for i in range(self.velocity):
-            dir_norm = self.direction.normalize()
-            next_x = self.position.x
-            next_y = self.position.y
+
+            next_x += dir_norm.x
+            next_y += dir_norm.y
 
             if next_x >= self.display.get_width() - self.size:
                 self.direction.x = -self.direction.x
                 break
 
-            if next_x <= 0 + self.size:
+            elif next_x <= 0 + self.size:
                 self.direction.x = abs(self.direction.x)
                 break
 
-            if next_y > self.display.get_height() - self.size:
+            elif next_y > self.display.get_height() - self.size:
                 self.direction.y = -self.direction.y
                 break
 
-            if next_y < 0 + self.size:
+            elif next_y < 0 + self.size:
                 self.direction.y = abs(self.direction.y)
                 break
 
-            next_x += dir_norm.x
-            next_y += dir_norm.y
+        self.move(next_x, next_y)
