16d0a4afb9889164529dd9151f3b9e1a31151975
martinlaursen97 <martin_laursen21@hotmail.com>
2022-08-15 02:02:44 +0200

    # check for collision between current and next position

--- ball.py
+++ ball.py
@@ -20,17 +20,28 @@ class Ball:
         pygame.draw.circle(display, (255, 255, 255), self.position, self.size)
 
     def check_collisions(self, display):
-        next_x = self.position.x + self.direction.x
-        next_y = self.position.y + self.direction.y
 
-        if next_x > display.get_width():
-            self.direction.x = -self.direction.x
+        # check for collision between current and next position
+        for i in range(self.velocity):
+            dir_norm = self.direction.normalize()
+            next_x = self.position.x
+            next_y = self.position.y
 
-        if next_x < 0:
-            self.direction.x = abs(self.direction.x)
+            if next_x >= display.get_width() - self.size:
+                self.direction.x = -self.direction.x
+                break
 
-        if next_y > display.get_height():
-            self.direction.y = -self.direction.y
+            if next_x <= 0 + self.size:
+                self.direction.x = abs(self.direction.x)
+                break
 
-        if next_y < 0:
-            self.direction.y = abs(self.direction.y)
+            if next_y > display.get_height() - self.size:
+                self.direction.y = -self.direction.y
+                break
+
+            if next_y < 0 + self.size:
+                self.direction.y = abs(self.direction.y)
+                break
+
+            next_x += dir_norm.x
+            next_y += dir_norm.y
